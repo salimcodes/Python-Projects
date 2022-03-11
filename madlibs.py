761ba1a90854d5fd68620f397473edee85fffa76
Salim Oyinlola <salimoyinlola@gmail.com>
2022-03-11 01:17:58 +0100

    Added madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+#Code written by Salim O. Oyinlola
+
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+adj = input("Input adjective: ")
+famous_person = input("Input Famous Person: ")
+
+madlibs = "Computer programming is so " + adj + ". It makes me so excited all the time because I love to " + verb1 + ". Stay hydrated and " + verb2 + " like you are " + famous_person + "!"
+
+print(madlibs)
