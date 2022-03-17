83003434e0ee8c5639abf16e5889ff537e232357
Salim Oyinlola <salimoyinlola@gmail.com>
2022-03-17 05:21:30 +0100

    Added TicTacToe file for game

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -1,4 +1,30 @@
 import os
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        #Letter can be X or O
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        pass
 
 os.system("PAUSE")
 
