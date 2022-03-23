960da42556d2c3c0d0ce53cfdc5d0ce0aba3098e
Salim Oyinlola <salimoyinlola@gmail.com>
2022-03-23 09:26:42 +0100

    Adding more

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -3,26 +3,83 @@
 import random
 import os
 
-def play():
-    user = input("What is your choice; 'r' for rock, 'p' for paper, 's' for scissors! \n")
-    computer = random.choice(['r', 'p', 's'])
 
-    if user == computer:
-        return 'It is a tie!'
+def play_game():
+    """Play one round of the game"""
 
-    #Given that r>s, s>p and p>r
+    while True:
+        try:
+            games = int(input("How many games? "))
+            if games >= 15: # Setting a limit of 15 games
+                print("Sorry, but we aren't allowed to play that many.")
+            else:
+                break
 
-    if is_win(user, computer):
-        return 'You won!'
-    else:
-        return 'You lost!'
+        except ValueError:
+            print("Please enter a number.")
 
-def is_win(player, opponent):
-    #returns true if player wins
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
-        return True
-    
-print(play())
+    won_computer = 0
+    won_human = 0
+
+    for game in range(games):
+        print("\nGame number", game + 1)
+        guess_computer = random.randint(1, 3)
+        print("3=Rock...2=Scissors...1=Paper")
+        guess_human = 0
+        while guess_human == 0:
+            try:
+                guess_human = int(input("1...2...3...What's your choice "))
+                if guess_human not in [1, 2, 3]:
+                    guess_human = 0
+                    print("Invalid")
+
+            except ValueError:
+                print("Please enter a number.")
+        print("This is my choice...")
+        if guess_computer == 1:
+            print("...Paper")
+        elif guess_computer == 2:
+            print("...Scissors")
+        elif guess_computer == 3:
+            print("...Rock")
+        if guess_computer == guess_human:
+            print("Tie Game. No winner")
+        elif guess_computer > guess_human:
+            if guess_human != 1 or guess_computer != 3:
+                print("Wow! I win!!!")
+                won_computer = won_computer + 1
+            else:
+                print("You win!!!")
+                won_human = won_human + 1
+        elif guess_computer == 1:
+            if guess_human != 3:
+                print("You win!!!")
+                won_human = won_human + 1
+            else:
+                print("Wow! I win!!!")
+                won_computer = won_computer + 1
+    print("\nHere is the final game score:")
+    print("I have won", won_computer, "game(s).")
+    print("You have won", won_human, "game(s).")
+    print("and", games - (won_computer + won_human), "game(s) ended in a tie.")
+    print("\nThanks for playing!!\n")
+
+
+def main() -> None:
+    print("SALIM OYINLOLA'S GAME OF ROCK, SCISSORS, PAPER")
+
+    keep_playing = True
+    while keep_playing:
+
+        play_game()
+
+        keep_playing = input("Play again? (yes or no) ").lower().startswith("y")
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 os.system("PAUSE")
 
